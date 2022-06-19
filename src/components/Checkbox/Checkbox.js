3ab06852e7157629cec9053ce2182143a1f1aa69
TiffanyJung <Tiffany.Jung@ibm.com>
2022-06-19 22:19:37 +0800

    feat: change checkbox component css to scss and add class

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,26 +1,26 @@
-import  React,{useState,useMemo} from 'react';
-import './checkbox.css'
+import React, { useState, useMemo } from 'react';
+import './checkbox.scss'
 const INIT_OPTIONS = [
-        {label:'00-未派車',checked:false,disabled:false,value:0},
-        {label:'00-未派車',checked:false,disabled:true,value:1},
-        {label:'00-未派車',checked:false,disabled:false,value:2},
-        {label:'00-未派車',checked:false,disabled:false,value:3},
-        {label:'00-未派車',checked:false,disabled:false,value:4},
-        {label:'00-未派車',checked:false,disabled:false,value:5},
-        {label:'00-未派車',checked:false,disabled:false,value:6},
-        {label:'00-未派車',checked:false,disabled:false,value:7},
-        {label:'00-未派車',checked:false,disabled:true,value:8},
-        {label:'00-未派車',checked:false,disabled:false,value:9},
-        {label:'00-未派車',checked:false,disabled:false,value:10},
-        {label:'00-未派車',checked:false,disabled:true,value:11},
-        {label:'00-未派車',checked:false,disabled:false,value:12},
+    { label: '00-未派車', checked: false, disabled: false, value: 0 },
+    { label: '00-未派車', checked: false, disabled: true, value: 1 },
+    { label: '00-未派車', checked: false, disabled: false, value: 2 },
+    { label: '00-未派車', checked: false, disabled: false, value: 3 },
+    { label: '00-未派車', checked: false, disabled: false, value: 4 },
+    { label: '00-未派車', checked: false, disabled: false, value: 5 },
+    { label: '00-未派車', checked: false, disabled: false, value: 6 },
+    { label: '00-未派車', checked: false, disabled: false, value: 7 },
+    { label: '00-未派車', checked: false, disabled: true, value: 8 },
+    { label: '00-未派車', checked: false, disabled: false, value: 9 },
+    { label: '00-未派車', checked: false, disabled: false, value: 10 },
+    { label: '00-未派車', checked: false, disabled: true, value: 11 },
+    { label: '00-未派車', checked: false, disabled: false, value: 12 },
 
-    ]
+]
 
 const Checkbox = (props) => {
-    return(
-        <input 
-            type="checkbox" 
+    return (
+        <input
+            type="checkbox"
             checked={props.checked}
             disabled={props.disabled}
             onChange={props.onChange}
@@ -30,66 +30,67 @@ const Checkbox = (props) => {
 }
 
 const CheckboxRow = ({ checkboxProps, label }) => {
-  return(
-    <tr className={checkboxProps.checked && !checkboxProps.disabled ? 'is-checked' : ''}>
-        <td>
-          <Checkbox type="checkbox" {...checkboxProps}/>
-        </td>
-        <td></td>
-        <td>{label}</td>
-    </tr>
-  )
+    return (
+        <tr className={checkboxProps.checked && !checkboxProps.disabled ? 'is-checked' : ''}>
+            <td>
+                <Checkbox type="checkbox" {...checkboxProps} />
+            </td>
+            <td></td>
+            <td>{label}</td>
+        </tr>
+    )
 };
 
 const CheckboxTableBody = ({ options, onChange }) => {
-  return(
-    <tbody>
-        {options.map((option, idx) => (
-            <CheckboxRow
-                key={option.value}
-                checkboxProps={{
-                    checked: option.checked,
-                    disabled: option.disabled,
-                    onChange: e => onChange(e, idx),
-                    value: option.value,
-                }}
-                label={option.label}
-            />
-        ))}
-    </tbody>
-)};
+    return (
+        <tbody>
+            {options.map((option, idx) => (
+                <CheckboxRow
+                    key={option.value}
+                    checkboxProps={{
+                        checked: option.checked,
+                        disabled: option.disabled,
+                        onChange: e => onChange(e, idx),
+                        value: option.value,
+                    }}
+                    label={option.label}
+                />
+            ))}
+        </tbody>
+    )
+};
 
 
 const useCheckBoxOptions = () => {
-    const [checkboxOptions,setCheckboxOptions] = useState(INIT_OPTIONS);
-    const [batchStart,setbatchStart] = useState(-1);
-    const {checkedNum, checkboxActiveNum} = useMemo(
+    const [checkboxOptions, setCheckboxOptions] = useState(INIT_OPTIONS);
+    const [batchStart, setbatchStart] = useState(-1);
+    const { checkedNum, checkboxActiveNum } = useMemo(
         () => {
-            let checkedNum=0;
-            let checkboxActiveNum=0;
+            let checkedNum = 0;
+            let checkboxActiveNum = 0;
             checkboxOptions.forEach(option => {
-                if(!option.disabled){
-                  checkboxActiveNum++;
-                  if(option.checked) checkedNum++;
+                if (!option.disabled) {
+                    checkboxActiveNum++;
+                    if (option.checked) checkedNum++;
                 }
             })
-            return {checkedNum,checkboxActiveNum};
+            return { checkedNum, checkboxActiveNum };
         }, [checkboxOptions]);
     const isAll = checkedNum === checkboxActiveNum;
 
     const onChange = (e) => {
         const nowCheckedValue = e.target.value;
-        const start =  Math.min(batchStart,nowCheckedValue);
-        const end =  Math.max(batchStart,nowCheckedValue);
+        const start = Math.min(batchStart, nowCheckedValue);
+        const end = Math.max(batchStart, nowCheckedValue);
         setCheckboxOptions(prev => {
             let newState = [...prev];
-            if(e.nativeEvent.shiftKey && batchStart>-1){
-                newState.forEach((element,index) => {
-                    if(index > start && index <= end && !element.disabled){
+            if (e.nativeEvent.shiftKey && batchStart > -1) {
+                newState.forEach((element, index) => {
+                    if (index > start && index <= end && !element.disabled) {
                         newState[index].checked = e.target.checked;
                     }
                 })
-            }else{
+            } else {
                 newState[nowCheckedValue].checked = e.target.checked;
             }
             return newState;
@@ -97,10 +98,10 @@ const useCheckBoxOptions = () => {
         setbatchStart(nowCheckedValue);
     };
 
-    const onCheckAllChange =() => {
+    const onCheckAllChange = () => {
         setbatchStart(-1);
-        setCheckboxOptions(prev => prev.map( item => ({...item,checked: !item.disabled ? !isAll : item.checked})))
-      
+        setCheckboxOptions(prev => prev.map(item => ({ ...item, checked: !item.disabled ? !isAll : item.checked })))
+
     };
     return { checkboxOptions, onChange, onCheckAllChange, isAll };
 
@@ -108,17 +109,19 @@ const useCheckBoxOptions = () => {
 const CheckboxPage = () => {
     const { checkboxOptions, onChange, onCheckAllChange, isAll } = useCheckBoxOptions();
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th><Checkbox onChange={onCheckAllChange} checked={isAll}></Checkbox></th>
-                    <th></th>
-                    <th>狀態</th>
-                </tr>
-                
-            </thead>
-        <CheckboxTableBody options={checkboxOptions} onChange={onChange}></CheckboxTableBody>
-        </table>
+        <div className='component-checkbox'>
+            <table>
+                <thead>
+                    <tr>
+                        <th><Checkbox onChange={onCheckAllChange} checked={isAll}></Checkbox></th>
+                        <th></th>
+                        <th>狀態</th>
+                    </tr>
+
+                </thead>
+                <CheckboxTableBody options={checkboxOptions} onChange={onChange}></CheckboxTableBody>
+            </table>
+        </div>
     )
 }
 
